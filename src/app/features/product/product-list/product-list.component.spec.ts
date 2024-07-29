002f2f1dd53c6f83ca09849c59662d1d553a7aea
abdelali djebara <adjebara@sqli.com>
2024-07-30 00:19:11 +0100

    test: adapt product-list test

--- src/app/features/product/product-list/product-list.component.spec.ts
+++ src/app/features/product/product-list/product-list.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ProductService } from '../products.service';
 import { ProductListComponent } from './product-list.component';
 
 describe('ProductListComponent', () => {
@@ -8,7 +11,12 @@ describe('ProductListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductListComponent]
+      imports: [ProductListComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        ProductService
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductListComponent);
